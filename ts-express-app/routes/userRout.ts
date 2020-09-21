26a4607de946ad83bb24abf846be76517ec0c774
Murat <murat.byazrov@yandex.ru>
2020-09-21 15:10:43 +0300

    удалил лишнее

--- ts-express-app/routes/userRout.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-export const userRout = Router(); // используется в app.ts
-
-import { getUsers, createUser, putUser } from '../controllers/userController';
-
-userRout.get('/', getUsers);
-userRout.post('/', createUser);
-userRout.put('/:id', putUser);
